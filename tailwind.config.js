04c6eec98e98ee2feb09b96054fec0747ec85dbb
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-06-07 05:32:33 +0600

    tailwind cli

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        custom:['Tapestry']
+      },
+      height:{
+        128:'32rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
